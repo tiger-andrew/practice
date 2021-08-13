c41bf40c4be9bbb99b2276fc43c0446986ca7d73
tiger-andrew <88873938+tiger-andrew@users.noreply.github.com>
2021-08-13 14:57:53 +0900

    Add files via upload

---
+++ GitCommitTest.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        int cntNames = int.Parse(Console.ReadLine());
+        
+        // 名前とダメージが対応する辞書を作成
+        // 辞書では値の更新ができないため、連想配列を作成
+        // ハッシュテーブルのValueはobject型のため、加算ができない。
+        // やっぱ辞書型でないといけない。
+        var nameAndDmg = new Dictionary<string, int>();
+        
+        // 辞書に入力値を追加していく（ダメージは初期値0）
+        for(int i=0; i<cntNames; i++)
+        {
+            nameAndDmg.Add(Console.ReadLine(), 0);
+        }
+        
+        
+        int cntDmgs = int.Parse(Console.ReadLine());
+        
+        for(int i=0; i<cntDmgs; i++)
+        {
+            string[] tmpList = Console.ReadLine().Split(' ');
+            nameAndDmg[tmpList[0]] += int.Parse(tmpList[1]);
+        }
+        
+        string answerName = Console.ReadLine();
+        
+        // FirstOrDefault を使用することで値からキーを取得できます
+        var pair = nameAndDmg.FirstOrDefault( c => c.Key == answerName );
+
+        Console.WriteLine( pair.Value );
+    }
+}
