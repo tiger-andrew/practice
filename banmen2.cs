3f6a57e1be9258f9832713c072caec2e7eca92b2
tiger-andrew <88873938+tiger-andrew@users.noreply.github.com>
2021-09-13 21:15:51 +0900

    Create banmen2.cs

---
+++ banmen2.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] tmpNum = Console.ReadLine().Split(' ');
+        int h = int.Parse(tmpNum[0]);
+        int w = int.Parse(tmpNum[1]);
+        int n = int.Parse(tmpNum[2]);
+        
+        var masuList = new List<string>();
+        
+        for(int i=0; i < h; i++)
+        {
+            masuList.Add(Console.ReadLine());
+        }
+        
+        var zahyouDic = new Dictionary<int,int>();
+        
+        for(int i=0; i < n; i++)
+        {
+            var tmp = Console.ReadLine().Split(' ');
+            zahyouDic.Add(int.Parse(tmp[0]), int.Parse(tmp[1]));
+        }
+        
+        foreach(var ele in zahyouDic)
+        {
+            int y = ele.Key;
+            int x = ele.Value;
+            string targetLine = masuList[y];
+            
+            string convertedTargetLine = "";
+            
+            for(int i=0; i<targetLine.Length;i++)
+            {
+                char c = targetLine[i];
+                if(i==x)
+                {
+                    c = '#';
+                }
+                
+                convertedTargetLine += c.ToString();
+            }
+            
+            masuList[y] = convertedTargetLine;
+        }
+        
+
+        foreach(var ele in masuList)
+        {
+            Console.WriteLine(ele);
+        }
+    }
+}
