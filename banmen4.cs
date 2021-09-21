10f73fa3c5dc956ead9002554ab7d31156f32250
tiger-andrew <88873938+tiger-andrew@users.noreply.github.com>
2021-09-21 22:05:31 +0900

    Create banmen4.cs

---
+++ banmen4.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var tmp = Console.ReadLine().Split(' ');
+        int h = int.Parse(tmp[0]);
+        int w = int.Parse(tmp[1]);
+        
+        var masuList = new List<string>();
+        
+        for(int i=0; i < h; i++)
+        {
+            masuList.Add(Console.ReadLine());
+        }
+        
+        
+        for(int k=0; k<h; k++)
+        {
+            for(int i=0; i<w; i++)
+            {
+               
+            }
+        }
+    }
+}
