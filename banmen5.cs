9b6e5d450a69e6b14e8abc62ddd6b486a5added9
tiger-andrew <88873938+tiger-andrew@users.noreply.github.com>
2021-09-22 19:57:10 +0900

    Create banmen5.cs

---
+++ banmen5.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var tmp = Console.ReadLine().Split(' ');
+        int h = int.Parse(tmp[0]);
+        int w = int.Parse(tmp[1]);
+        
+        var masuList = new List<string>();
+        
+        for(int i=0; i < h; i++)
+        {
+            masuList.Add(Console.ReadLine());
+        }
+        
+        
+        // 縦の判定
+        for(int i=0; i<h; i++)
+        {
+            for(int j=0; j<w; j++)
+            {
+               if(i==0 || masuList[i-1][j].ToString()=="#")
+               {
+                   if(i==h-1 || masuList[i+1][j].ToString()=="#")
+                   {
+                       if(j == 0 && masuList[i][1].ToString() == "#")
+                        {
+                            Console.WriteLine(i + " " + j);
+                        }
+        
+                        if(j == w-1 && masuList[i][w-2].ToString() == "#")
+                        {
+                            Console.WriteLine(i + " " + j);
+                        }
+        
+                        if(j != 0 && j != w-1)
+                        {
+                            if(masuList[i][j-1].ToString() == "#" && masuList[i][j+1].ToString() == "#")
+                            {
+                                Console.WriteLine(i + " " + j);
+                            }
+                        }
+                   }
+               }
+            }
+        }
+    }
+}
